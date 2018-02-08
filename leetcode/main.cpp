73c076cf4a0a6173bca3a43e858418b46e3bc617
Weichao Zhou <zwc662@outlook.com>
2018-02-07 23:10:47 -0500

    Initial Commit

---
+++ leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by Weichao  Zhou on 2/7/18.
+//  Copyright © 2018 Weichao  Zhou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
